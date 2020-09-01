f5cd2bc9b2cfaf93b885b0ccef09b2abf8ef69d6
ben-norton <ben.norton@naturalsciences.org>
2020-09-01 10:48:58 -0400

    Additional Geospatial Data
    Add IUCN spatial data components

---
+++ js/ncmns-spatial-data.js
@@ -0,0 +1,10 @@
+/* NCMNS Data Publishing Spatial Data Requests */
+/* 20200828 */
+
+$.ajaxSetup({beforeSend: function(xhr) {
+	if (xhr.overrideMimeType) {
+		xhr.overrideMimeType("application/json");
+		}
+	}
+});
+
